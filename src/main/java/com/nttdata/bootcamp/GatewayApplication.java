ad889e608903527247b0a4ebdc81a911c74da3b0
jbenavidesg27 <jbenavidesg27@gmail.com>
2022-08-04 17:43:12 -0500

    CheckStyle terminado

--- src/main/java/com/nttdata/bootcamp/GatewayApplication.java
+++ src/main/java/com/nttdata/bootcamp/GatewayApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Gateway Application.
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
 
 }
